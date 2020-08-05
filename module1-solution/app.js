5d837cd959ee931a9d35210efbdcab948c99521e
changyufei <changyufei@live.cn>
2020-08-05 22:41:41 +0800

    six commit

--- module1-solution/app.js
+++ module1-solution/app.js
@@ -1,26 +1,27 @@
-(funtion () {
-	'use strict';
-
-	angular.module('LunchCheck',[])
-	.controller('LunchCheckController',LunchCheckController);
-
-	LunchCheckController.$inject = ['$scope'];
-
-	function LunchCheckController("$scope") {
-		var message = "";
-		$scope.check = function () {
-			if ($scope.items != NULL) {
-				var items = $scope.items.split(,)
-				if (items.length <= 3) {
-					message = "Enjoy!";
-				} else {
-					message = "Too much!";
-				}
-			} else {
-				message = "Please enter data first";
-			}
-		}
-		
-	}
-
+(function () {
+  'use strict';
+  
+  angular.module('LunchCheck', [])
+  .controller('LunchCheckController', LunchCheckController);
+  
+  LunchCheckController.$inject = ['$scope'];
+  
+  function LunchCheckController($scope) {
+    $scope.items = '';
+    $scope.check = function () {
+      let txt = $scope.items;
+      let txtArr = txt.split(',');
+      for (let i = 0; i < txtArr.length; i++ ) {
+        if (txtArr[i].trim() === '' ) {//check if any item is empty
+          $scope.name = 'Please enter data first';
+          return;
+        }
+      }
+      if (txtArr.length >= 1 && txtArr.length <= 3) {//Enjoy!
+        $scope.name = 'Enjoy!';
+      } else {//too much
+        $scope.name = 'Too much!';
+      }
+    }
+  }
 })();
