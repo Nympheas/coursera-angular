546895112fbce1895543051816426153e079f6c1
changyufei <changyufei@live.cn>
2020-08-05 18:53:46 +0800

    third commit

--- module1-solution/app.js
+++ module1-solution/app.js
@@ -4,6 +4,25 @@
 	angular.module('LunchCheck',[])
 	.controller('LunchCheckController',LunchCheckController);
 
-	LunchCheckController.$inject = ['$scope']
+	LunchCheckController.$inject = ['$scope'];
+
+	function LunchCheckController("$scope") {
+		var message = "";
+		$scope.check = function () {
+			if ($scope.items != NULL) {
+				var items = $scope.items.split(,)
+				if (items.length <= 3) {
+					message = "Enjoy!";
+				} else {
+					message = "Too much!";
+				}
+			} else {
+				message = "Please enter data first";
+			}
+
+			return message;
+		}
+		
+	}
 
 })();
