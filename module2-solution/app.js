fb20180a60d732f15d12913790bdad11fcca021e
changyufei <changyufei@live.cn>
2020-08-24 21:47:52 +0800

    to buy

---
+++ module2-solution/app.js
@@ -0,0 +1,37 @@
+(function () {
+  'use strict';
+  
+  angular.module('ShoppingListCheckOff', [])
+  .controller('ToBuyController', ToBuyController);
+  .controller('AlreadyBoughtController', AlreadyBoughtController);
+  .service('ShoppingListCheckOffService',ShoppingListCheckOffService);
+  
+  ToBuyController.$inject = ['ShoppingListCheckOffService'];
+  AlreadyBoughtController.$inject = ['ShoppingListCheckOffService'];
+  
+  function ToBuyController(ShoppingListCheckOffService) {
+    var ToBuy = this;
+    ToBuy.removeItem = function (itemIndex) {
+    	ShoppingListCheckOffService.removeItem(itemIndex);
+    }
+  }
+  function AlreadyBoughtController(ShoppingListCheckOffService) {
+    
+  }
+
+  function ShoppingListCheckOffService() {
+    var service = this;
+    var ToBuyItems = [{ name: "cookies", quantity: 10 },
+    { name: "cookies", quantity: 10 },{ name: "cookies", quantity: 10 },
+    { name: "cookies", quantity: 10 },{ name: "cookies", quantity: 10 }];
+    var boughtItems = [];
+
+    service.removeItem = function (itemIndex) {
+    	var item = ToBuyItems[itemIndex];
+        boughtItems.push(item);
+        ToBuyItems.splice(itemIndex, 1);
+	  };
+    }
+
+  }
+})();
